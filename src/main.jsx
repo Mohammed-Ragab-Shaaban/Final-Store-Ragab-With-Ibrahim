6a8467def3edc2a378ca0e3b37fb7b148856e2f3
Ibrahim-Sarhan <samamedym33@gmail.com>
2024-09-19 12:20:02 +0300

    Second branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
-import { HeaderStateProviderContext } from './Pages/Components/Context.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./index.css";
+import { HeaderStateProviderContext } from "./Pages/Components/Context.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <HeaderStateProviderContext>
-        <App />
-    </HeaderStateProviderContext>
-)
+createRoot(document.getElementById("root")).render(
+  <HeaderStateProviderContext>
+    <App />
+  </HeaderStateProviderContext>
+);
